e29c92754091b4e0f72560202988d1c2a27d81c1
Naomi Bryant <naomi_bryant@hotmail.co.uk>
2023-11-11 14:42:26 +0000

    used the rest api exercises IOU files as a template for the weather  APIs - error messages as follows:  Nopplication could not be started as  there was no  org.springframework.boot.web.servlet.server.ServletWebServerFactory  bean defined in the context.  active profile set, falling back  to 1 default profile: "default" and

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -1,22 +1,34 @@
 package com.cbfacademy.apiassessment;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(WeatherController.class, args);
+	//	WeatherController test = new WeatherController();
+		
 	}
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
-	}
 
-}
+//	@GetMapping("/greeting")
+//	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+//		return String.format("Hello %s", name);
+	}
+	
+		
+	}
+	
+
+
